93c99995a4aa3223232aaac701157147047dc239
shunking-tech <shnktech@gmail.com>
2019-07-21 17:41:37 +0900

    Update group#create

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,7 +6,12 @@ class GroupsController < ApplicationController
   end
 
   def create
-    group = Group.create(group_params)
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end
   end
 
   def edit
