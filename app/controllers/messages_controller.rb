e047e49496c3f026c55276b26882b8311f82a270
shunking-tech <shnktech@gmail.com>
2019-07-22 17:22:31 +0900

    use form_for

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,8 @@ class MessagesController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @group = Group.find(params[:group_id])
+    @message = Message.new
   end
 
 end
