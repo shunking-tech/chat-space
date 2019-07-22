701f561ca1406909b47c10a0a9c37595367b81e1
shunking-tech <shnktech@gmail.com>
2019-07-22 17:27:57 +0900

    Revert "Revert "Revert "use form_for"""
    This reverts commit 96f82e38ab591403d424dbc343dc59fb89bcfd46.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,8 +2,6 @@ class MessagesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @group = Group.find(params[:group_id])
-    @message = Message.new
   end
 
 end
