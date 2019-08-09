697744785cc2fd0e5280357526b91c1274f1451c
shunking-tech <shnktech@gmail.com>
2019-08-09 19:55:17 +0900

    Update users_controller.rb
    ログイン中のユーザーは検索で出てこないように処理

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.where("name LIKE ?", "%#{params[:user]}%")
+    @users = User.where("name LIKE ?", "%#{params[:user]}%").where.not("id = #{current_user.id}")
     respond_to do |format|
       format.html
       format.json
