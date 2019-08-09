5ad176689a0e3e8acb451506558a7ad798d9c048
shunking-tech <shnktech@gmail.com>
2019-08-09 19:38:10 +0900

    Update users_controller.rb
    userの検索結果の表示をhtmlとjsonで分岐させる

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
 
   def index
     @users = User.where("name LIKE ?", "%#{params[:user]}%")
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
 
   def edit
