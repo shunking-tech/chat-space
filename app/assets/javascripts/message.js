cb39214ca0ea4a247f3a6430b17da101d1081429
shunking-tech <shnktech@gmail.com>
2019-08-11 04:01:45 +0900

    Update message.js
    webAPIを用いて最新のメッセージ情報を取得

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,7 @@
 $(function(){
   function buildHTML(message){
     var content = (message.content != null) ? `<p>${message.content}</p>` : ``;
-    var image = (message.image != null) ? `<img src="${message.image}">` : ``;
+    var image = (message.image.url != null) ? `<img src="${message.image.url}">` : ``;
     var html = `<section id="message" data-message-id="${message.id}">
                   <div class="upper-info">
                     <div class="upper-info__user">
@@ -43,4 +43,28 @@ $(function(){
       $('.contents-right__bottom__form__submit').attr('disabled', false);
     })
   });
+
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    var m = document.getElementsByTagName('section');
+    console.log(m)
+    last_message_id = m[m.length -1].getAttribute('data-message-id');
+    console.log(last_message_id)
+    $.ajax({
+      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+      url: 'api/messages',
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      console.log('sucsess');
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
+  setInterval(reloadMessages, 5000);
 });
