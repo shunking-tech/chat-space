7b6d6e20091e30a6655815af748f5d7198aa2251
shunking-tech <shnktech@gmail.com>
2019-08-24 19:04:08 +0900

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module ChatSpace
       g.test_framework false
     end
     config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    # Time.zone.now.strftime("%Y%m%d")
   end
 end
