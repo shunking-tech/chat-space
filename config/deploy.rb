02bc40855b574e4bd4bf70e3985fe8bbaed356ed
shunking-tech <shnktech@gmail.com>
2019-08-24 15:31:16 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,7 +30,7 @@ set :default_env, {
   rbenv_root: "/usr/local/rbenv",
   path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
   AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
-  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"],
   SECRET_BASE_KEY: ENV["SECRET_KEY_BASE"] 
 }
 
